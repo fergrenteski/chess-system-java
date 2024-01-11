6e0821bc27d200ebd5d53314bed624d64d179943
Luiz Grenteski <luiz.grenteski@rrs-software.com.br>
2024-01-11 12:00:45 -0300

    Project created

---
+++ chess-system/src/main/java/application/Program.java
@@ -0,0 +1,13 @@
+
+package application;
+
+/**
+ *
+ * @author luiz.grenteski
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
